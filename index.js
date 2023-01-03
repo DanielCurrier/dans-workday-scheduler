9e1ec195b6f4b971e869cded6f343beafdead86c
Dan Currier <dcurrier585@gmail.com>
2023-01-03 17:49:22 -0500

    added clear button with local storage deletion

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 
 const store = window.localStorage;
-
 const container = $(".container");
 
 const now = moment();
+const currentTime = now.format("HH");
+
 $('#currentDay').text(now.format("dddd MMMM DD, YYYY"));
+
+$("#clearFieldsBtn").click((event) => {
+    event.preventDefault;
+    $("textarea").val("");
+    localStorage.clear();
+});
 
 
 
